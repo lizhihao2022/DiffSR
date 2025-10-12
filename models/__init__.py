276cdf70b74c172b74e0ae2e1208cceba9af63f8
Yi Chuang <462008633@qq.com>
2025-10-12 18:49:18 +0000

    operators,transformer类模型适配；ERA5数据集适配

--- models/__init__.py
+++ models/__init__.py
@@ -2,10 +2,18 @@ from .fno import FNO2d
 from .unet import UNet2d
 from . import ddpm
 from . import sr3
+from .galerkin import Galerkin_Transformer
+from .MWT import MWT_SuperResolution
+from .sronet import SRNO
+from .swin_Transformer import SwinSR
     
 _model_dict = {
     "FNO2d": FNO2d,
     "UNet2d": UNet2d,
+    "Galerkin_Transformer": Galerkin_Transformer,
+    "MWT2d": MWT_SuperResolution,
+    "SRNO": SRNO,
+    "Swin_Transformer": SwinSR,
     "DDPM": {
         "model": ddpm.UNet,
         "diffusion": ddpm.GaussianDiffusion,
